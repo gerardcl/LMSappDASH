ddc13af947ba4a0abb9dedc4d073bc80234f8ef2
Gerard CL <gerard.castillo@i2cat.net>
2015-07-29 15:27:36 +0200

    forms organization ready to be applyed

--- app/app.js
+++ app/app.js
@@ -5,41 +5,28 @@ $(document).ready( function() {
 		lmsDashRepresentation = null,
 		lmsDashRepresentationList = null;
 
+    ////////////////////////////////////////////
+    // EVENTS MANAGEMENT
+    ////////////////////////////////////////////    
     $("#view").load("./app/views/instance.html");
 
-    $( '#view, #form, #connectForm' ).submit(function(event) {
-	  	var $form = $( '#connectForm' );
-		var	message = { 
-			'host' : $form.find( "input[id='host']" ).val(),
-			'port' : $form.find( "input[id='port']" ).val()
-		};
-	    var	uri = apiURI + $form .attr('action');
-        $.ajax({
-            type: 'POST',
-            url: uri,
-            data: message,
-            dataType: 'json',
-        	success : function(msg) {
-				if(!msg.error){
-					lmsInstance = message;
-                    addAlertSuccess(msg.message);
-                    $("#disconnectButton").removeClass("hidden");
-				    $("#view").load("./app/views/input.html");
-				} else {
-					lmsInstance = null;
-  				    addAlertError(msg.error);
-  				}
-			},
-			error : function(xhr, msg) {
-				lmsInstance = null;
-			    addAlertError('ERROR: \
-			    ' + msg + ' - ' + xhr.responseText+ ' - No API available');
-			}
-        })
-        console.log('lmsInstance:')
-        console.log(lmsInstance)        
-        return false;
-	});
+    $( document ).on( "submit", "form", function() {
+        var form = $(this);
+
+        console.log(form)
+        console.log(form.context.id)
+
+        switch (form.context.id){
+            case 'connectForm':
+                connectForm(form);
+                break;
+            case 'setRTMPForm':
+                setRTMPForm(form);
+                break;
+            default:
+                addAlertError('ERROR: no form available');
+        }
+    });
 
     $('#disconnectButton').on('click', function(event) {
         bootbox.confirm("Are you sure to disconnect? This implies restarting the app...", function(result) {
@@ -68,7 +55,74 @@ $(document).ready( function() {
         });
     });
 
-    // ALERTS MANAGEMENT
+    ////////////////////////////////////////////
+    // FORMS METHODS
+    ////////////////////////////////////////////
+    function connectForm(form) {
+        var message = { 
+            'host' : form.find( "input[id='host']" ).val(),
+            'port' : form.find( "input[id='port']" ).val()
+        };
+
+        var uri = apiURI +'/connect';
+        $.ajax({
+            type: 'POST',
+            url: uri,
+            data: message,
+            dataType: 'json',
+            success : function(msg) {
+                if(!msg.error){
+                    lmsInstance = message;
+                    addAlertSuccess(msg.message);
+                    $("#disconnectButton").removeClass("hidden");
+                    $("#view").load("./app/views/input.html");
+                } else {
+                    lmsInstance = null;
+                    addAlertError(msg.error);
+                }
+            },
+            error : function(xhr, msg) {
+                lmsInstance = null;
+                addAlertError('ERROR: \
+                ' + msg + ' - ' + xhr.responseText+ ' - No API available');
+            }
+        })
+    };
+
+    function setRTMPForm(form) {
+        var params = { 
+            'type' : 'demuxer',
+            'uri' : form.find( "input[id='uri']" ).val()
+        };
+
+        var uri = apiURI +'/connect';
+        $.ajax({
+            type: 'POST',
+            url: uri,
+            data: message,
+            dataType: 'json',
+            success : function(msg) {
+                if(!msg.error){
+                    lmsInstance = message;
+                    addAlertSuccess(msg.message);
+                    $("#disconnectButton").removeClass("hidden");
+                    $("#view").load("./app/views/input.html");
+                } else {
+                    lmsInstance = null;
+                    addAlertError(msg.error);
+                }
+            },
+            error : function(xhr, msg) {
+                lmsInstance = null;
+                addAlertError('ERROR: \
+                ' + msg + ' - ' + xhr.responseText+ ' - No API available');
+            }
+        })
+    };
+
+    ////////////////////////////////////////////
+    // ALERTS METHODS
+    ////////////////////////////////////////////
     function addAlertError(message) {
         var id = 'lmsError';
         var JQueryId = "#" + id;
